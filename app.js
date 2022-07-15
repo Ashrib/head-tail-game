2f9bd466429809d1ba18d37866deedb3a9ba2c06
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-07-15 20:23:46 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
+var optionDiv = document.getElementById("options-box");
+optionDiv.style.display = "none";
 
 var selectedUser;
+var userNmbr = Math.round(Math.random()*1);
 function userToss() {
-    var userNmbr = Math.round(Math.random()*1)
     switch(userNmbr) {
         case 0 :
-            console.log("User1");
             selectedUser = "User 1";
             break;
         case 1 :
-            console.log("User2");
             selectedUser = "User 2";
             break;
     }
     document.getElementById("selected-user").innerText = selectedUser + " can choose the option.";
-}
+    optionDiv.style.display = "block";
+};
+
+var usersDiv = document.getElementById("users");
+usersDiv.style.display = "none";
+
+function optionClick(a) {
+    usersDiv.style.display = "flex";
+
+    var selectedOption;
+    var randomSelected;
+    if(a === "head") {
+        selectedOption = "Head";
+        randomSelected = "Tail";
+    }
+    else{
+        selectedOption = "Tail";
+        randomSelected = "Head";
+    };
+
+    if(selectedUser === "User 1") {
+        document.getElementById("hh").innerText = selectedOption;
+        document.getElementById("gg").innerText = randomSelected;
+    }
+    else{
+        
+        document.getElementById("hh").innerText = randomSelected;
+        document.getElementById("gg").innerText = selectedOption;
+    };
+};
+var user1_point = 0;
+var user2_point = 0;
+document.getElementById("user1-points").innerText = user1_point;
+document.getElementById("user2-points").innerText = user2_point;
